cc239091596318cb2937cf50b6b8836547dfd035
orchsik <orchsik@naver.com>
2022-03-28 13:55:15 +0900

    Add: 홈화면 좌측 행정구역 셀렉트드랍박스

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -11,10 +11,17 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<Map<String, String>> datas = [];
+  late String currentLocation;
+  final Map<String, String> locationTypeToString = {
+    "ara": "아라동",
+    "ora": "오라동",
+    "donam": "도남동"
+  };
 
   @override
   void initState() {
     super.initState();
+    currentLocation = "ara";
     datas = [
       {
         "cid": "1",
@@ -110,13 +117,30 @@ class _HomeState extends State<Home> {
         onTap: () {
           print("click");
         },
-        child: Row(
-          children: [
-            Text(
-              "아라동",
-            ),
-            Icon(Icons.arrow_drop_down)
-          ],
+        child: PopupMenuButton<String>(
+          offset: Offset(0, 28),
+          shape: ShapeBorder.lerp(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              1),
+          onSelected: (String where) {
+            setState(() {
+              currentLocation = where;
+            });
+          },
+          itemBuilder: (BuildContext context) {
+            return [
+              PopupMenuItem(value: "ara", child: Text("아라동")),
+              PopupMenuItem(value: "ora", child: Text("오라동")),
+              PopupMenuItem(value: "donam", child: Text("도남동")),
+            ];
+          },
+          child: Row(
+            children: [
+              Text(locationTypeToString[currentLocation]!),
+              Icon(Icons.arrow_drop_down)
+            ],
+          ),
         ),
       ),
       elevation: 1,
