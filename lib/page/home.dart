2407f728dabbd91aa3ba0c10d3b33c7ae18a095a
orchsik <orchsik@naver.com>
2022-03-28 12:35:49 +0900

    Add: 바텀네비게이션

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -11,6 +11,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<Map<String, String>> datas = [];
+  int _currentPageIndex = 0;
 
   @override
   void initState() {
@@ -206,14 +207,48 @@ class _HomeState extends State<Home> {
         itemCount: 10);
   }
 
+  BottomNavigationBarItem _bottomNavigationBarItem(
+      String iconName, String label) {
+    return BottomNavigationBarItem(
+      icon: Padding(
+        padding: const EdgeInsets.only(bottom: 5),
+        child: SvgPicture.asset(
+          "assets/svg/${iconName}_off.svg",
+          width: 22,
+        ),
+      ),
+      label: label,
+    );
+  }
+
+  Widget _bottomNavigationBarWidget() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      onTap: (int index) {
+        print(index);
+        setState(() {
+          _currentPageIndex = index;
+        });
+      },
+      currentIndex: _currentPageIndex,
+      selectedItemColor: Colors.black,
+      selectedLabelStyle: TextStyle(color: Colors.black),
+      selectedFontSize: 12,
+      items: [
+        _bottomNavigationBarItem("home", "홈"),
+        _bottomNavigationBarItem("notes", "동네생활"),
+        _bottomNavigationBarItem("location", "내 근처"),
+        _bottomNavigationBarItem("chat", "채팅"),
+        _bottomNavigationBarItem("user", "나의 당근"),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _appbarWidget(),
-      body: _bodyWidget(),
-      // bottomNavigationBar: Container(
-      //   color: Colors.red,
-      // ),
-    );
+        appBar: _appbarWidget(),
+        body: _bodyWidget(),
+        bottomNavigationBar: _bottomNavigationBarWidget());
   }
 }
