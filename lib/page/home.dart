cbef281697d81a7321db2de298ffd06f2bc292c1
orchsik <orchsik@naver.com>
2022-03-28 16:29:22 +0900

    Add: 데이터 fetch 임시 구현

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,3 +1,4 @@
+import 'package:carrot_market/repository/contents_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
@@ -10,8 +11,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<Map<String, String>> datas = [];
   late String currentLocation;
+  late ContentRepository contentRepository;
   final Map<String, String> locationTypeToString = {
     "ara": "아라동",
     "ora": "오라동",
@@ -22,93 +23,21 @@ class _HomeState extends State<Home> {
   void initState() {
     super.initState();
     currentLocation = "ara";
-    datas = [
-      {
-        "cid": "1",
-        "image": "assets/images/ara-1.jpg",
-        "title": "네메시스 축구화275",
-        "location": "제주 제주시 아라동",
-        "price": "30000",
-        "likes": "2"
-      },
-      {
-        "cid": "2",
-        "image": "assets/images/ara-2.jpg",
-        "title": "LA갈비 5kg팔아요~",
-        "location": "제주 제주시 아라동",
-        "price": "100000",
-        "likes": "5"
-      },
-      {
-        "cid": "3",
-        "image": "assets/images/ara-3.jpg",
-        "title": "치약팝니다",
-        "location": "제주 제주시 아라동",
-        "price": "5000",
-        "likes": "0"
-      },
-      {
-        "cid": "4",
-        "image": "assets/images/ara-4.jpg",
-        "title": "[풀박스]맥북프로16인치 터치바 스페이스그레이",
-        "location": "제주 제주시 아라동",
-        "price": "2500000",
-        "likes": "6"
-      },
-      {
-        "cid": "5",
-        "image": "assets/images/ara-5.jpg",
-        "title": "디월트존기임팩",
-        "location": "제주 제주시 아라동",
-        "price": "150000",
-        "likes": "2"
-      },
-      {
-        "cid": "6",
-        "image": "assets/images/ara-6.jpg",
-        "title": "갤럭시s10",
-        "location": "제주 제주시 아라동",
-        "price": "180000",
-        "likes": "2"
-      },
-      {
-        "cid": "7",
-        "image": "assets/images/ara-7.jpg",
-        "title": "선반",
-        "location": "제주 제주시 아라동",
-        "price": "15000",
-        "likes": "2"
-      },
-      {
-        "cid": "8",
-        "image": "assets/images/ara-8.jpg",
-        "title": "냉장 쇼케이스",
-        "location": "제주 제주시 아라동",
-        "price": "80000",
-        "likes": "3"
-      },
-      {
-        "cid": "9",
-        "image": "assets/images/ara-9.jpg",
-        "title": "대우 미니냉장고",
-        "location": "제주 제주시 아라동",
-        "price": "30000",
-        "likes": "3"
-      },
-      {
-        "cid": "10",
-        "image": "assets/images/ara-10.jpg",
-        "title": "멜킨스 풀업 턱걸이 판매합니다.",
-        "location": "제주 제주시 아라동",
-        "price": "50000",
-        "likes": "7"
-      },
-    ];
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    contentRepository = ContentRepository();
   }
 
   final oCcy = NumberFormat("#,###", "ko_KR");
   String calcStringToWon(String priceString) {
-    return "${oCcy.format(int.parse(priceString))}원";
+    if (int.tryParse(priceString) != null) {
+      return "${oCcy.format(double.parse(priceString))}원";
+    } else {
+      return "무료나눔";
+    }
   }
 
   PreferredSizeWidget _appBarWidget() {
@@ -157,77 +86,100 @@ class _HomeState extends State<Home> {
     );
   }
 
-  Widget _bodyWidget() {
+  _loadContents() {
+    return contentRepository.loadContentFromLocation(currentLocation);
+  }
+
+  _makeDataList(List<Map<String, String>> datas) {
     return ListView.separated(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        itemBuilder: (BuildContext _context, int index) {
-          return Container(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    child: Image.asset(
-                      datas[index]["image"]!,
-                      width: 100,
-                      height: 100,
-                    ),
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      itemBuilder: (BuildContext _context, int index) {
+        return Container(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  child: Image.asset(
+                    datas[index]["image"]!,
+                    width: 100,
+                    height: 100,
                   ),
-                  Expanded(
-                    child: Container(
-                      padding: const EdgeInsets.only(left: 20),
-                      height: 100,
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              datas[index]["title"]!,
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  overflow: TextOverflow.ellipsis),
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.only(left: 20),
+                    height: 100,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            datas[index]["title"]!,
+                            style: TextStyle(
+                                fontSize: 15, overflow: TextOverflow.ellipsis),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            datas[index]["location"]!,
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black.withOpacity(0.5),
                             ),
-                            SizedBox(height: 5),
-                            Text(
-                              datas[index]["location"]!,
-                              style: TextStyle(
-                                fontSize: 12,
-                                color: Colors.black.withOpacity(0.5),
-                              ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            calcStringToWon(datas[index]["price"]!),
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          SizedBox(height: 5),
+                          Expanded(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                SvgPicture.asset(
+                                  "assets/svg/heart_off.svg",
+                                  width: 13,
+                                  height: 13,
+                                ),
+                                SizedBox(width: 5),
+                                Text(datas[index]["likes"]!)
+                              ],
                             ),
-                            SizedBox(height: 5),
-                            Text(
-                              calcStringToWon(datas[index]["price"]!),
-                              style: TextStyle(fontWeight: FontWeight.w500),
-                            ),
-                            SizedBox(height: 5),
-                            Expanded(
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  SvgPicture.asset(
-                                    "assets/svg/heart_off.svg",
-                                    width: 13,
-                                    height: 13,
-                                  ),
-                                  SizedBox(width: 5),
-                                  Text(datas[index]["likes"]!)
-                                ],
-                              ),
-                            ),
-                          ]),
-                    ),
-                  )
-                ],
-              ));
-        },
-        separatorBuilder: (BuildContext _context, int index) {
-          return Container(
-            height: 1,
-            color: Colors.black.withOpacity(0.4),
-          );
-        },
-        itemCount: 10);
+                          ),
+                        ]),
+                  ),
+                )
+              ],
+            ));
+      },
+      separatorBuilder: (BuildContext _context, int index) {
+        return Container(
+          height: 1,
+          color: Colors.black.withOpacity(0.4),
+        );
+      },
+      itemCount: 10,
+    );
+  }
+
+  Widget _bodyWidget() {
+    return FutureBuilder(
+      future: _loadContents(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (!snapshot.hasData) {
+          return Center(child: Text("해당 지역에 데이터가 없습니다."));
+        }
+        if (snapshot.hasError) {
+          return Center(child: Text("데이터 오류"));
+        }
+
+        return _makeDataList(snapshot.data);
+      },
+    );
   }
 
   @override
